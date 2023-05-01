e2038576cdd03ee20f4ef64ff20a9bf08f6e84eb
nrp-silva <nickolascaragua@gmail.com>
2023-05-01 19:55:28 -0300

    Introdução a mocking e TDD e criação do caso de uso do check in

---
+++ src/repositories/in-memory/in-memory-check-ins-repository.ts
@@ -0,0 +1,41 @@
+import { CheckIn, Prisma } from '@prisma/client'
+import { randomUUID } from 'node:crypto'
+import { CheckInsRepository } from '../check-ins-repository'
+import dayjs from 'dayjs'
+
+export class InMemoryCheckInsRepository implements CheckInsRepository {
+  public items: CheckIn[] = []
+
+  async findByUserIdOnDate(userId: string, date: Date) {
+    const startOfTheDay = dayjs(date).startOf('date')
+    const endOfTheDay = dayjs(date).endOf('date')
+
+    const checkInOnSameDate = this.items.find((checkIn) => {
+      const checkInDate = dayjs(checkIn.created_at)
+      const isOnSameDate =
+        checkInDate.isAfter(startOfTheDay) && checkInDate.isBefore(endOfTheDay)
+
+      return checkIn.user_id === userId && isOnSameDate
+    })
+
+    if (!checkInOnSameDate) {
+      return null
+    }
+
+    return checkInOnSameDate
+  }
+
+  async create(data: Prisma.CheckInUncheckedCreateInput) {
+    const checkIn = {
+      id: randomUUID(),
+      created_at: new Date(),
+      validate_at: data.validate_at ? new Date(data.validate_at) : null,
+      user_id: data.user_id,
+      gym_id: data.gym_id,
+    }
+
+    this.items.push(checkIn)
+
+    return checkIn
+  }
+}
